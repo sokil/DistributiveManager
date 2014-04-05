5316fe9b1522c93da749f1ee56c3a93e70c91e28
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-04-05 13:38:40 +0300

    fix specifying APPLICATION_ENV var

--- bootstrap.py
+++ bootstrap.py
@@ -10,7 +10,7 @@ app.register_blueprint(api)
 # Configure app
 app.config.from_object('configs.default.Config')
 
-env = os.environ['APPLICATION_ENV'] or 'development'
+env = os.getenv('APPLICATION_ENV', 'development')
 app.config.from_object('configs.' + env + '.Config')
 
 # Run app
