5300f21e2ca1e0968ee74220808937669f52f516
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-04-06 14:22:11 +0300

    implement fetching data from db

--- routes/site.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, current_app, url_for
-from flask_login import login_required
-
-site = Blueprint('site', __name__)
-
-@site.route("/")
-@login_required
-def index():
-    return 'index'
